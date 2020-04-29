2ecbc84e8a6148c44e7f20a7e1c4113aad6e58e9
nikeshkamble25 <nikeshkamble25@gmail.com>
2020-04-29 13:18:08 +0530

    Update index.js

--- src/_actions/index.js
+++ src/_actions/index.js
@@ -3,12 +3,10 @@ import { ADD_TYPE } from '../_shared/types';
 export const addTask = (task) => {
 	// setInterval(() => {
 	return function (dispatch) {
-		setInterval(() => {
-            dispatch({
+	    dispatch({
                 type: ADD_TYPE,
                 newTask: task,
             })
-        }, 3000);
 	};
 
 	// }, 3000);
