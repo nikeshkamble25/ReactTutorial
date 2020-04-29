11fa923afbeca2da828670099791e82152c41cfd
nikeshkamble25 <nikeshkamble25@gmail.com>
2020-04-29 15:41:13 +0530

    Update Master.js

--- src/master/Master.js
+++ src/master/Master.js
@@ -6,7 +6,7 @@ import * as actions from '../_actions';
 import { connect } from 'react-redux';
 
 class App extends React.Component {
-	Title = 'Task Manager V1';
+	Title = 'Task Manager V2';
 	constructor() {
 		super();
 		this.AddTask = this.AddTask.bind(this);
